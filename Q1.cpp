ff229da2643a89ab2a10c96907f0e4514281b89c
Prathmesh Chhabra <p.chhabra2002@gmail.com>
2022-04-15 20:41:10 +0530

    Q1 -- static array changed to dynamic

--- Q1.cpp
+++ Q1.cpp
@@ -26,7 +26,7 @@ int main()
     int size;
     cout<<"Enter the size of array: ";
     cin>>size;
-    int arr[size];
+    int * arr = new int[size];
     cout<<"Enter the elements in the array"<<endl;
     for(int i = 0; i<size; i++)
     {
